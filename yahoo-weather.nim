2ebf37c403c55b6bed8c443df85089beb556efe9
achesak <achesak@yahoo.com>
2013-10-03 17:10:53 -0500

    First commit.

---
+++ yahoo-weather.nim
@@ -0,0 +1,12 @@
+# Yahoo! Weather API wrapper.
+
+# Written by Adam Chesak.
+# Code released under the MIT open source license.
+
+# Import modules.
+import httpclient
+import cgi
+import strutils
+import xmlparser
+import xmltree
+import streams
